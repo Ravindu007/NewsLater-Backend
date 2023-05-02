bc13b783288d397bc9879735cbf570bcff0233cc
Ravindu007 <ravindu0504@gmail.com>
2023-05-02 08:21:20 +0530

    change nature of controller

--- controllers/basicControllers.js
+++ controllers/basicControllers.js
@@ -22,18 +22,19 @@ const getAllData = async(req, res) => {
 
   // make a post request to mailchimp 
   try{
-      const response = await fetch(`https://us21.api.mailchimp.com/3.0/lists/9e65659159`,{
-      method:"POST",
-      headers:{
-        'Authorization':`Bearer 91ee1934c1c0fb1d3302ab9cf97e76b6-us21`
-      },
-      body:jsonData
-      })
-      const json = await response.json()
+      // const response = await fetch(`https://us21.api.mailchimp.com/3.0/lists/9e65659159`,{
+      // method:"POST",
+      // headers:{
+      //   'Authorization':`Bearer 91ee1934c1c0fb1d3302ab9cf97e76b6-us21`
+      // },
+      // body:jsonData
+      // })
+      // const json = await response.json()
 
-      if(response.ok){
-        res.status(200).json({msg:"ok"}) //sending res to the client side 
-      }
+      // if(response.ok){
+      // }
+      
+      res.status(200).json({msg:"ok"}) //sending res to the client side 
   }catch(error){
     res.status(400).json({error:error.message})
   }
